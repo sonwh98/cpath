2459514d6127744007db599f9f0d51290cf29310
SonnyTo <sto@csicorporatecard.com>
2014-02-04 15:48:19 -0500

    empty commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cpath
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/cpath/core_test.clj
@@ -0,0 +1,7 @@
+(ns cpath.core-test
+  (:require [clojure.test :refer :all]
+            [cpath.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
